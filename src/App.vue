5b84e5ff483573012fadd56f13424785adbbc61e
guohaiyang <1517366319@qq.com>
2024-11-12 23:09:21 +0800

    ‘Flex：增加新的颜色文件’

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: guohaiyang 1517366319@qq.com
  * @Date: 2024-11-11 14:23:31
  * @LastEditors: guohaiyang 1517366319@qq.com
- * @LastEditTime: 2024-11-11 21:59:19
+ * @LastEditTime: 2024-11-12 22:37:25
  * @FilePath: /ocean-vite3-elementPlus/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -47,7 +47,8 @@ watchEffect(() => {
 </script>
 
 <template>
-  <el-config-provider :locale="zhCN" namespace="ocean">
+  <!-- namespace="ocean" -->
+  <el-config-provider :locale="zhCN">
     <router-view v-slot="{ Component }">
       <component :is="Component" />
     </router-view>
